86e8a497d5a605efc234d6a379e02ef85f92f8bd
D_rwasa <rwasamanzidavid@gmail.com>
2023-10-09 15:12:59 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+elp", "copyright", "credits" or "license()" for more information.
+>>> print("hello world")
+hello world
+>>> name='david'
+>>> print("hello am "+name+"!!")
+hello am david!!
+
+
+year=2023
